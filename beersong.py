b7580fd9272b1d061acc23c4e3a7e94b9086eb8d
sooraj <srjps202@gmail.com>
2016-09-02 11:12:37 +0530

    BeerSong

---
+++ beersong.py
@@ -0,0 +1,9 @@
+print 100*'_'
+print " '99 Bottles of Beer on the wall'  Lyrics : "
+n=2
+print 100*'_'
+for i in xrange(99,n-1,-1):
+	print i,'bottles of beer on the wall, ',i,' bottles of beer.\n Take one down and pass it around,',i-1,' bottles of beer on the wall.\n'
+
+print " 1 bottle of beer on the wall, 1 bottle of beer.\n Take it down and pass it around, no more bottles of beer on the wall.\n \n No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall.\n "
+
